7a2088de4e6c3efb2459303b74e68683f26c7ddb
OuniYoussef <ouniyoussef619@gmail.com>
2025-03-10 01:41:11 +0100

    final version task rename

---
+++ database/models/task.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: false, trim: true },
+    status: {
+        type: String,
+        enum: ["in progress", "completed", "cancelled"],
+        default: "in progress"
+    },
+    createdAt: { type: Date, default: Date.now, immutable: true },
+    assignedUser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model("Task", taskSchema);
